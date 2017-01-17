0e5ed937ea2a582ff4533a9f91a99d1c669c5261
Sathish S <ssathish_mca@live.com>
2017-01-17 20:12:43 +0530

    added headers

--- sendmemail.php
+++ sendmemail.php
@@ -7,6 +7,10 @@ $mail = $_POST['email'];
 $subject = $_POST['subject']; 
 $message = $_POST['message']; 
 $to = $youremail; 
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type:text/html; charset=UTF-8' . "\r\n";
+$headers .= "From: ".$_POST['name']."<".$_POST['Email'].">\r\n"; 
+$headers .= "Reply-To: ".$_POST["email"]."\r\n";
 $mailsubject = 'Messsage recived for'.$fromsubject.' Contact Page';
 $body = $fromsubject.'
 	
@@ -18,7 +22,7 @@ $body = $fromsubject.'
 	 '.$message.'	
 	|---------END MESSAGE----------|'; 
 echo "Thank you fo your feedback. I will contact you shortly if needed.<br/>Go to <a href='/index.php'>Home Page</a>"; 
-								mail($to, $subject, $body);
+								mail($to, $subject, $body,$headers);
  } else { 
 echo "You must write a message. </br> Please go to <a href='/index.html'>Home Page</a>"; 
 }
